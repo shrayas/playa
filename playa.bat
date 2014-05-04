9db1ae8629d5ee3fc00ac4516d1e956a29cb9d5e
Karthik <krthkv26@gmail.com>
2014-05-04 20:40:02 +0530

    add batch file for windows

---
+++ playa.bat
@@ -0,0 +1,9 @@
+:: you can add this location to PATH and use RUN to run playa commands
+
+@echo off
+
+:: change dir to the location of the file
+cd /d %~dp0
+
+:: start via the venv python
+start /B env\Scripts\python.exe src\playa.py %*
